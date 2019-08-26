a08814937cc4cc30a9bdc8806d4ef2cfc6bec457
Kevin Tou <toukevin@gmail.com>
2019-08-26 15:23:54 -0700

    Set up redux files

---
+++ src/store/auth/reducers.js
@@ -0,0 +1,14 @@
+const initialState = {
+  errors: "",
+  isLoading: "",
+  isAuth: false,
+};
+
+const reducers = (state = initialState, { type, payload }) => {
+  switch (type) {
+    default:
+      return state;
+  }
+};
+
+export default reducers;
---
+++ src/store/reviews/reducers.js
@@ -0,0 +1,14 @@
+const initialState = {
+  reviews: [],
+  error: "",
+  isLoading: false,
+};
+
+const reducers = (state = initialState, { type, payload }) => {
+  switch (type) {
+    default:
+      return state;
+  }
+};
+
+export default reducers;
