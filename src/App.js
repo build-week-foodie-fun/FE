160f6bdc73396b04f97bbeac02509de7007efdea
Jojo Zhang <jojozhang0327@gmail.com>
2019-08-26 22:33:40 -0400

    complete signUp and Login forms before styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from "react";
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import PrivateRoute from "./utils/PrivateRoute";
 import "./App.css";
+import FormikLoginForm from "./components/Login";
+import FormikSignUpForm  from "./components/SignUpForm";
 
 const App = () => {
   return (
     <>
+      {/* <Login /> */}
+
       <Router>
-        <h1>App</h1>
-        <Switch>{/* Insert routes */}</Switch>
+        <Switch>
+          <Route exact path="/" component={FormikLoginForm} />
+          <Route path="/signup" component={FormikSignUpForm} />
+        </Switch>
       </Router>
     </>
   );
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -3,3 +3,83 @@ import axios from "axios";
 import { Form, Field, withFormik } from "formik";
 import * as Yup from "yup";
 
+const SignUpForm = ({ errors, touched, values, status }) => {
+    const [users, setUsers] = useState([]);
+    console.log(touched)
+    useEffect(()=> {
+      if (status) {
+          setUsers([...users, status]);
+      }
+    }, [status])
+
+  return(
+    <div>
+      <a href="#">
+        <img className="logo" src={require("../img/Foodie_Icon.png")} alt="Foodie Fun logo" />
+      </a>
+      <h1>Foodie Fun</h1>
+      
+      <Form>
+        <label>Username
+            <Field type="text" name="userName" placeholder="FoodieFun" />
+        </label>
+
+        <label>Email
+          <Field type="text" name="email" placeholder="food@foodiefun.com" />
+        </label>
+        {touched.email && errors.email && (
+          <p className="error">{errors.email}</p>
+        )}
+
+        <label>Password
+          <Field type="password" name="password" placeholder="Password" />
+        </label>
+        {touched.password && errors.password && (
+          <p className="error">{errors.password}</p>
+        )}
+
+        <label>Confirm Password
+          <Field type="password" name="confirmPassword" placeholder="Confirm Password" />
+        </label>
+        {touched.password && errors.password && (
+          <p className="error">{errors.password}</p>
+        )}
+
+        <button type="submit">Submit</button>
+      </Form>            
+    </div>
+    )
+}
+
+const FormikSignUpForm = withFormik({
+  mapPropsToValues({ userName,email, password, confirmPassword  }) {
+    return {
+        userName: userName || "",
+        email: email || "",
+        password: password || "",
+        confirmPassword: confirmPassword || ""
+    };
+  },
+
+  validationSchema: Yup.object().shape({
+      email: Yup.string().email().required("Please enter your email."),
+      password: Yup.string().min(6).required("Please enter at least 6 letters"),
+      confirmPassword: Yup.string().required().test("passwords match", "Passwords must match", function(value) {
+          return this.parent.password ===value;
+      })
+  }),
+
+  handleSubmit(values, { setStatus }) {
+      axios
+          .post('#', values)
+          .then(res => {
+              console.log(res)
+              setStatus(res);
+          })
+          .catch(err => console.log(err.response));
+  }
+
+})(SignUpForm)
+
+
+export default FormikSignUpForm;
