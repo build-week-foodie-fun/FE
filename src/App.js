8876c832282aa282c82da66e7f20e4c631f46c18
karenjli <karenjy.li@gmail.com>
2019-08-27 15:39:40 -0500

    profile component added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import PrivateRoute from "./utils/PrivateRoute";
 //Components
 import NavBar from "./components/NavBar";
 import FormikLoginForm from "./components/Login";
-import FormikSignUpForm  from "./components/SignUpForm";
+import FormikSignUpForm from "./components/SignUpForm";
+import Profile from "./components/Profile";
 
 //Material-ui
-import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import Container from "@material-ui/core/Container";
 
@@ -21,44 +22,45 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const theme = createMuiTheme({
-    typography: {
-        fontFamily:  '-apple-system,system-ui,BlinkMacSystemFont,' +
-            '"Montserrat", "Nunito"',
+  typography: {
+    fontFamily:
+      "-apple-system,system-ui,BlinkMacSystemFont," + '"Montserrat", "Nunito"',
+  },
+  palette: {
+    primary: {
+      main: "#D80000",
+      light: "#ff0000",
+      dark: "#ab0000",
+      contrastText: "white",
     },
-    palette: {
-        primary: {
-            main: "#D80000",
-            light: '#ff0000',
-            dark: '#ab0000',
-            contrastText: 'white',
-        },
-        secondary: {
-            main: '#C6C6C6',
-            light: '#EDEDED',
-            dark: '#7C7C7C',
-            contrastText: 'white',
-        },
+    secondary: {
+      main: "#C6C6C6",
+      light: "#EDEDED",
+      dark: "#7C7C7C",
+      contrastText: "white",
     },
+  },
 });
 
 const App = () => {
-    const classes = useStyles();
-    return (
-        <>
-            <MuiThemeProvider theme={theme}>
-                <Router>
-                    <NavBar/>
-                    <Container maxWidth="md" className={classes.root}>
-                        <h1>App</h1>
-                        <Switch>
-                            <Route path="/login" component={FormikLoginForm} />
-                            <Route path="/signup" component={FormikSignUpForm} />
-                        </Switch>
-                    </Container>
-                </Router>
-            </MuiThemeProvider>
-        </>
-    );
+  const classes = useStyles();
+  return (
+    <>
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          <NavBar />
+          <Container maxWidth="md" className={classes.root}>
+            <h1>App</h1>
+            <Profile />
+            <Switch>
+              <Route path="/login" component={FormikLoginForm} />
+              <Route path="/signup" component={FormikSignUpForm} />
+            </Switch>
+          </Container>
+        </Router>
+      </MuiThemeProvider>
+    </>
+  );
 };
 
 export default App;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,23 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-export default function Profile () {
-    const [userPicture, setUserPicture] = ();
+export default function Profile(props) {
+  const [userPicture, setUserPicture] = [];
 
-    return(
-        <div className="user-profile">
-            <div className="navBar">
-                
-            </div>
+  return (
+    <div className="user-profile">
+      <div className="navBar">
+        <img src="/public/hamburger-menu.svg"></img>
+      </div>
 
-            <div className="user-picture">
-                <img src={props}></img>
-            </div>
-            
-        </div>
-    )
+      <div className="user-picture">
+        <img src="{props}"></img>
+      </div>
+      <div className="user-name">
+        <h2>{props.name}</h2>
+      </div>
+      <div className="add-btn">
+        <button>+</button>
+      </div>
+    </div>
+  );
 }
