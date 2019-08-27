b5b2d9d2c88d3ac65734e3afb4f924c2c777477d
Kevin Tou <toukevin@gmail.com>
2019-08-27 10:39:51 -0700

    Added register functionality

--- src/store/auth/authActions.js
+++ src/store/auth/authActions.js
@@ -1,29 +1,51 @@
 import axios from "axios";
 
-import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT } from "./types";
+import {
+  LOGIN_START,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  REGISTER_START,
+  REGISTER_SUCCESS,
+  REGISTER_FAILURE,
+} from "./types";
 
 //login action
 
 export const login = (credentials, history) => {
-    return dispatch => {
-        dispatch({type: LOGIN_START});
-        axios.post("/auth/login", credentials)
-            .then(res => {
-                dispatch({type: LOGIN_SUCCESS, payload: res.data});
-                // set token to local storage (review console logs for correct token path)
-                localStorage.setItem('token', res.data.token)
-                //route to profile
-                history.push("/profile")
-            })
-            .catch(err => {
-                dispatch({type: LOGIN_FAIL, payload: err.response});
-            })
-    }
-}
+  return dispatch => {
+    dispatch({ type: LOGIN_START });
+    axios
+      .post("/auth/login", credentials)
+      .then(res => {
+        dispatch({ type: LOGIN_SUCCESS, payload: res.data });
+        // set token to local storage (review console logs for correct token path)
+        localStorage.setItem("token", res.data.token);
+        //route to profile
+        history.push("/profile");
+      })
+      .catch(err => {
+        dispatch({ type: LOGIN_FAIL, payload: err.response });
+      });
+  };
+};
 
 export const logout = () => {
-    return dispatch => {
-        dispatch({type: LOGOUT });
-        localStorage.removeItem('token')
-    }
-}
+  return dispatch => {
+    dispatch({ type: LOGOUT });
+    localStorage.removeItem("token");
+  };
+};
+
+export const register = user => dispatch => {
+  dispatch({ type: REGISTER_START });
+
+  axios
+    .post("/auth/register", user)
+    .then(res => {
+      dispatch({ type: REGISTER_SUCCESS });
+    })
+    .catch(err => {
+      dispatch({ type: REGISTER_FAILURE, payload: err.response });
+    });
+};
