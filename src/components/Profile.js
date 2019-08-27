ac84ceb7df222241c7a441f09d8ef1d70c3206ca
karenjli <karenjy.li@gmail.com>
2019-08-27 17:59:14 -0500

    Profile built

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,23 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import hamburger from "../img/menu-icon.png";
+import Add from "@material-ui/icons/Add";
+import FilterListIcon from "@material-ui/icons/FilterList";
+import IconButton from "@material-ui/core/IconButton";
+import axios from "axios";
 
 export default function Profile(props) {
   const [userPicture, setUserPicture] = [];
+  const [userName, setUserName] = [];
+  useEffect(() => {
+    axios
+      .get("https://buildweek-foodie1.herokuapp.com/auth/api/users/id")
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }, []);
 
   return (
     <div className="user-profile">
-      <div className="navBar">
-        <img src="/public/hamburger-menu.svg"></img>
-      </div>
-
       <div className="user-picture">
-        <img src="{props}"></img>
+        <img src=""></img>
       </div>
       <div className="user-name">
-        <h2>{props.name}</h2>
+        <h2>{props.username}</h2>
       </div>
       <div className="add-btn">
-        <button>+</button>
+        <IconButton>
+          <Add color="primary" />
+        </IconButton>
       </div>
+      <div className="viewBar">
+        <FilterListIcon />
+      </div>
+      <div className="reviewList"></div>
     </div>
   );
 }
