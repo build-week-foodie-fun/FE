bb2fab89b1a289ee397ea4e311cfe9a279ea0af9
karenjli <karenjy.li@gmail.com>
2019-08-27 14:33:02 -0500

    review form built

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import PrivateRoute from "./utils/PrivateRoute";
+import ReviewForm from "./components/ReviewForm.js";
 
 //Components
 import NavBar from "./components/NavBar";
 import FormikLoginForm from "./components/Login";
-import FormikSignUpForm  from "./components/SignUpForm";
+import FormikSignUpForm from "./components/SignUpForm";
 
 //Material-ui
-import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import Container from "@material-ui/core/Container";
 
@@ -21,44 +22,45 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const theme = createMuiTheme({
-    typography: {
-        fontFamily:  '-apple-system,system-ui,BlinkMacSystemFont,' +
-            '"Montserrat", "Nunito"',
+  typography: {
+    fontFamily:
+      "-apple-system,system-ui,BlinkMacSystemFont," + '"Montserrat", "Nunito"',
+  },
+  palette: {
+    primary: {
+      main: "#D80000",
+      light: "#ff0000",
+      dark: "#ab0000",
+      contrastText: "white",
     },
-    palette: {
-        primary: {
-            main: "#D80000",
-            light: '#ff0000',
-            dark: '#ab0000',
-            contrastText: 'white',
-        },
-        secondary: {
-            main: '#C6C6C6',
-            light: '#EDEDED',
-            dark: '#7C7C7C',
-            contrastText: 'white',
-        },
+    secondary: {
+      main: "#C6C6C6",
+      light: "#EDEDED",
+      dark: "#7C7C7C",
+      contrastText: "white",
     },
+  },
 });
 
 const App = () => {
-    const classes = useStyles();
-    return (
-        <>
-            <MuiThemeProvider theme={theme}>
-                <Router>
-                    <NavBar/>
-                    <Container maxWidth="md" className={classes.root}>
-                        <h1>App</h1>
-                        <Switch>
-                            <Route path="/login" component={FormikLoginForm} />
-                            <Route path="/signup" component={FormikSignUpForm} />
-                        </Switch>
-                    </Container>
-                </Router>
-            </MuiThemeProvider>
-        </>
-    );
+  const classes = useStyles();
+  return (
+    <>
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          <NavBar />
+          <Container maxWidth="md" className={classes.root}>
+            <h1>App</h1>
+            <ReviewForm />
+            <Switch>
+              <Route path="/login" component={FormikLoginForm} />
+              <Route path="/signup" component={FormikSignUpForm} />
+            </Switch>
+          </Container>
+        </Router>
+      </MuiThemeProvider>
+    </>
+  );
 };
 
 export default App;
--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,3 +1,48 @@
 import React from "react";
-import { Formik, Form, Label } from "formik";
+import { Formik, Form, Field } from "formik";
 import axios from "axios";
+
+export default function ReviewForm() {
+  return (
+    <div className="review-form">
+      <Formik>
+        <Form>
+          <label className="restaurant_name" html for="Name">
+            Restaurant
+            <Field type="text" name="restaurant_name" />
+          </label>
+          <label className="restaurant_type" html for="type">
+            Restaurant Type
+            <Field type="text" name="restaurant_type" />
+          </label>
+          <label className="food_item" html for="food-item">
+            Food Item
+            <Field type="text" name="food_item" />
+          </label>
+          <label className="date" html for="date">
+            Date
+            <Field type="date" name="date" />
+          </label>
+          <label className="food_price" html for="price">
+            Price
+            <Field type="text" name="food_price" />
+          </label>
+          <label className="food_rating" html for="rating">
+            Rating
+            <Field type="number" min="1" max="5" name="food_rating" />
+          </label>
+          <Field
+            type="text"
+            name="restaurant_type"
+            placeholder="Upload image"
+          />
+          <label className="comments" html for="comments">
+            Other Comments
+            <Field type="text" name="restaurant_type" />
+          </label>
+          <button className="submitBtn">Submit</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
