7889699111804dc7a8571e1db723475da5d32021
karenjli <karenjy.li@gmail.com>
2019-08-27 15:12:22 -0500

    profile page components added

---
+++ src/components/Profile.js
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+
+export default function Profile () {
+    const [userPicture, setUserPicture] = ();
+
+    return(
+        <div className="user-profile">
+            <div className="navBar">
+                
+            </div>
+
+            <div className="user-picture">
+                <img src={props}></img>
+            </div>
+            
+        </div>
+    )
+}
