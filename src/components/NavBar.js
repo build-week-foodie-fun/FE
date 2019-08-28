9f94c4fb0fba5440ad80c933d63537d49af01625
Sasha <mix.filipov@mail.ru>
2019-08-28 13:15:38 -0700

    isauth is working

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -51,10 +51,10 @@ const NavBar = props => {
                     <Typography variant="h6" className={classes.title}>
                         Foodie Fun
                     </Typography>
-                    {props.isAuth && <LinkButton component={RouterLink} to="/login">Login</LinkButton>}
-                    {props.isAuth && <LinkButton component={RouterLink} to="/signup">Sign up</LinkButton>}
-                    {!props.isAuth && <LinkButton component={RouterLink} onClick={() => props.logout()}>Logout</LinkButton>}
-                    {!props.isAuth && <LinkButton component={RouterLink} to="/profile/reviewForm">Create Review</LinkButton>}
+                    {!props.isAuth && <LinkButton component={RouterLink} to="/login">Login</LinkButton>}
+                    {!props.isAuth && <LinkButton component={RouterLink} to="/signup">Sign up</LinkButton>}
+                    {props.isAuth && <LinkButton component={RouterLink} onClick={() => props.logout()}>Logout</LinkButton>}
+                    {props.isAuth && <LinkButton component={RouterLink} to="/profile/reviewForm">Create Review</LinkButton>}
 
                 </Toolbar>
             </AppBar>
@@ -64,7 +64,7 @@ const NavBar = props => {
 
 const mapPropsToState = state => {
     return {
-        isAuth: state.isAuth,
+        isAuth: state.auth.isAuth,
     }
 };
 export default connect(mapPropsToState, {logout})(NavBar);
