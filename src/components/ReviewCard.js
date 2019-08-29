2d99a189d67c1c6dc02f687fa6c11eada7416be7
Sasha <mix.filipov@mail.ru>
2019-08-28 18:35:28 -0700

    signle review is connected

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
 
 export default function ReviewCard (props) {
   const { id, resName, itemName, itemImgUrl, foodRating, price} = props;
   return (
-    <div>
-      <img src={itemImgUrl} alt={itemName} />
+    <Card>
+        <CardMedia
+            image={itemImgUrl}
+            />
+      {/*<img src={itemImgUrl} alt={itemName} />*/}
+      <CardContent>
+      </CardContent>
       <h3>{resName}</h3>
       <p>{itemName}</p>
       <h3>{price}</h3>
@@ -13,6 +21,6 @@ export default function ReviewCard (props) {
       <Link to={`/profile/review/${id}`}>
         <h4>...</h4>
       </Link>
-    </div>
+    </Card>
   )
 }
