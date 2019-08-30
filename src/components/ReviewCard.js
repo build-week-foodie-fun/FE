fc7ccf4b556d2de0b0070801a2234ebdbaaf07d0
Sasha <mix.filipov@mail.ru>
2019-08-29 22:27:02 -0700

    btn is ready

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -1,29 +1,69 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import { Card, Icon, Image } from 'semantic-ui-react';
+import {Card, Icon, Image} from 'semantic-ui-react';
+import IconButton from "@material-ui/core/IconButton";
+import {Clear} from "@material-ui/icons";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import {connect} from "react-redux";
+import {deleteReview} from "../store/reviews/reviewsActions";
 
-export default function ReviewCard(props) {
+
+const useStyles = makeStyles({
+    deleteIcon: {
+        borderTop: "1.5px solid #EDEDED",
+    },
+    iconContainer: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    delete: {
+        width: 42,
+        height: 30,
+        paddingBottom: 28,
+        border: '1px solid #EDEDED',
+        '&:hover': {
+            width: 42,
+            height: 30,
+            paddingBottom: 28,
+            border: '1px solid #EDEDED',
+            color: 'red',
+            background: 'white',
+        },
+    }
+});
+
+
+ function ReviewCard(props) {
+    const classes = useStyles();
     const {id, resName, itemName, itemImgUrl, foodRating, price} = props;
     console.log(props)
     return (
-          <Card key={id}>
-            <Image src={itemImgUrl} wrapped ui={false} />
+        <Card key={id}>
+            <Image src={itemImgUrl} wrapped ui={false}/>
 
             <Card.Content>
-              <Card.Header>{resName}</Card.Header>
-              <Card.Meta>{itemName}</Card.Meta>
-              <Card.Description>
-                {`Price: $${price}`} <br />
-                {`Rating: ${foodRating}`}
-              </Card.Description>
+                <Card.Header>{resName}</Card.Header>
+                <Card.Meta>{itemName}</Card.Meta>
+                <Card.Description>
+                    {`Price: $${price}`} <br/>
+                    {`Rating: ${foodRating}`}
+                </Card.Description>
             </Card.Content>
-                
+
             <Card.Content extra>
-              <Link to={`/profile/review/${id}`}>
-                <Icon name='linkify' />
-                More Info
-              </Link>
+                <div className={classes.iconContainer}>
+                    <Link to={`/profile/review/${id}`}>
+                        <Icon name='linkify'/>
+                        More Info
+                    </Link>
+                    <IconButton className={classes.delete} onClick={() => props.deleteReview(id)}>
+                        <Clear/>
+                    </IconButton>
+                </div>
             </Card.Content>
-          </Card>
+        </Card>
     )
 }
+
+export default connect(null ,{deleteReview})(ReviewCard);
